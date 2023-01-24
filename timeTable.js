3435217dce5bfcfa24796e88bb59ded6bb95d556
Mohd_ Ahsan Khan <97978224+mahsank111@users.noreply.github.com>
2023-01-24 19:44:00 +0530

    Add files via upload

---
+++ timeTable.js
@@ -0,0 +1,171 @@
+const Sunday =[
+    {   
+        time: 'Sunday',
+        roomNumber: 'Holiday',
+        subject: 'No class Enjoy😊',
+        type: ''
+    }
+]
+const Monday =[
+    {   
+        time: '10-11 AM',
+        roomNumber: '38-605E',
+        subject: 'CAP213',
+        type: 'Lecture'
+    },
+    {   
+        time: '11-12 AM',
+        roomNumber: '38-605E',
+        subject: 'CAP214',
+        type: 'Lecture'
+    },
+    {   
+        time: '12-01 PM',
+        roomNumber: '38-605E',
+        subject: 'CAP267',
+        type: 'Lecture'
+    },
+    {   
+        time: '03-04 PM',
+        roomNumber: '38-801',
+        subject: 'ECE281',
+        type: 'Lecture'
+    },
+    {   
+        time: '04-05 PM',
+        roomNumber: '38-801',
+        subject: 'PES209',
+        type: 'Lecture'
+    }
+
+]
+const Tuesday =[
+    {   
+        time: '10-11 AM',
+        roomNumber: '38-605E',
+        subject: 'CAP213',
+        type: 'Tutorial'
+    },
+    {   
+        time: '11-12 AM',
+        roomNumber: '38-605E',
+        subject: 'CAP214',
+        type: 'Lecture'
+    },
+    {   
+        time: '12-01 PM',
+        roomNumber: '38-605E',
+        subject: 'CAP267',
+        type: 'Lecture'
+    },
+    {   
+        time: '03-04 PM',
+        roomNumber: '38-801',
+        subject: 'ECE281',
+        type: 'Lecture'
+    },
+    {   
+        time: '12-01 PM',
+        roomNumber: '38-801',
+        subject: 'PES209',
+        type: 'Lecture'
+    }
+
+]
+
+const Wednesday =[
+    {   
+        time: '10-11 AM',
+        roomNumber: '38-605E',
+        subject: 'CAP213',
+        type: 'Lecture'
+    },
+    {   
+        time: '11-12 AM',
+        roomNumber: '38-605E',
+        subject: 'CAP214',
+        type: 'Lecture'
+    },
+    {   
+        time: '12-01 PM',
+        roomNumber: '38-605E',
+        subject: 'CAP267',
+        type: 'Lecture'
+    },
+    {   
+        time: '03-04 PM',
+        roomNumber: '38-801',
+        subject: 'ECE281',
+        type: 'Lecture'
+    },
+    {   
+        time: '12-01 PM',
+        roomNumber: '38-801',
+        subject: 'PES209',
+        type: 'Tutorial'
+    }
+]
+
+const Thursday =[
+    {   
+        time: '11-12 AM',
+        roomNumber: '38-605E',
+        subject: 'CAP213',
+        type: 'Lecture'
+    },
+    {   
+        time: '12-01 PM',
+        roomNumber: '38-612',
+        subject: 'CAP282',
+        type: 'Practical'
+    },
+    {   
+        time: '02-03 PM',
+        roomNumber: '38-612',
+        subject: 'CAP283',
+        type: 'Practical'
+    },
+    {   
+        time: '03-04 PM',
+        roomNumber: '33-110',
+        subject: 'ECE281',
+        type: 'Practical'
+    },
+    {   
+        time: '04-05 PM',
+        roomNumber: '38-801',
+        subject: 'PES209',
+        type: 'Tutorial'
+    }
+]
+
+const Friday =[
+    {   
+        time: '12-01 AM',
+        roomNumber: '38-612',
+        subject: 'CAP282',
+        type: 'Practical'
+    },
+    {   
+        time: '01-02 PM',
+        roomNumber: '38-612',
+        subject: 'CAP283',
+        type: 'Practical'
+    },
+    {   
+        time: '03-04 PM',
+        roomNumber: '38-605E',
+        subject: 'CAP214',
+        type: 'Lecture'
+    },
+
+]
+
+const Saturday =[
+    {   
+        time: 'N/A',
+        roomNumber: 'N/A',
+        subject: 'CHECK MAKEUP CLASSES 😒',
+        type: 'N/A'
+    }
+]
